f62d13e2c9cf7e9990435bb733f5c3608a3e12f0
henryjuho <juhob@hotmail.co.uk>
2017-02-24 19:26:44 +0000

    Script to test model with 1 class of sites

---
+++ test_anavar1.1_1class.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+
+
+def read_sim_sfs(sfs_file):
+    sfs_line = ',\t'.join(open(sfs_file).readlines()[0].rstrip('\n').split())
+    return sfs_line
+
+
+def main():
+
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', help='Sample size', required=True)
+    parser.add_argument('-t1', help='Theta 1', required=True)
+    parser.add_argument('-g1', help='Gamma 1', required=True)
+    parser.add_argument('-e1', help='Error 1', required=True)
+    parser.add_argument('-o', help='Output dir and file prefix', required=True)
+    args = parser.parse_args()
+
+    # variables
+    n = args.n
+    theta = args.t1
+    gamma = args.g1
+    e = args.e1
+    out = args.o
+    sfs_file = out + '.sfs.txt'
+    control_file = out + '.control.txt'
+    results_file = out + '.results.txt'
+    log_file = out + '.log.txt'
+
+    # simulate data and read it in
+    sim_cmd = 'wolfram -script snpdfe_spike_gen.withopts.m ' + n + ' ' + theta + ' ' + gamma + ' ' + e + ' ' + sfs_file
+    subprocess.call(sim_cmd, shell=True)
+    sfs = read_sim_sfs(sfs_file)
+
+    # write control file
+    control_contents = ('num_sets: 1\n'
+                        'search_algorithm: NLOPT_LD_SLSQP\n'
+                        'maxeval: 100000\n'
+                        'maxtime: 600\n'
+                        'num_searches: 100\n'
+                        'nnoimp: 3\n'
+                        'maximp: 5\n\n'
+                        'begin[data_1]\n'
+                        'type: snp\n'
+                        'n: ' + n + '\n'
+                        'm: 1\n'
+                        'folded: false\n'
+                        'sfs: ' + sfs + '\n'
+                        'dfe: discrete\n'
+                        'c: 1\n'
+                        'theta_range: 1, 10000\n'
+                        'gamma_range: -500, 100\n'
+                        'e_range: 0, 1\n'
+                        'end[data_1]')
+    with open(control_file, 'w') as control:
+        control.write(control_contents)
+
+    # submit jobs
+    anavar_cmd = 'anavar1.1 ' + control_file + ' ' + results_file + ' ' + log_file
+    subprocess.call(anavar_cmd, shell=True)
+
+if __name__ == '__main__':
+    main()
