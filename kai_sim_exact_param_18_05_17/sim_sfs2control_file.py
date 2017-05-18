e25cb78ebabd549843e8d72f3721227889f647fa
henryjuho <juhob@hotmail.co.uk>
2017-05-18 09:21:44 +0100

    script to insert sfs from stdin into control file

---
+++ kai_sim_exact_param_18_05_17/sim_sfs2control_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+# arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-control_file', help='control file to use as template', required=True)
+args = parser.parse_args()
+
+# variables
+c_file = args.control_file
+
+counter = 0
+for sfs_line in sys.stdin:
+    counter += 1
+    with open(c_file[:c_file.rfind('.')] + '.rep' + str(counter) + '.control.txt') as out_control:
+        for line in open(c_file):
+            if line.startswith('sfs'):
+                out_control.write('sfs: ' + sfs_line + '\n')
+            else:
+                out_control.write(line)
