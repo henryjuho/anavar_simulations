481c68f2c885e5d9615add53391f8826a1d3c6bb
henryjuho <juhob@hotmail.co.uk>
2017-05-23 09:49:29 +0100

    correct wd

--- kai_sim_exact_param_18_05_17/parallel_anavar_runs.py
+++ kai_sim_exact_param_18_05_17/parallel_anavar_runs.py
@@ -2,12 +2,16 @@
 
 import sys
 from qsub import *
+import os
 
 for line in sys.stdin:
+
+    pwd = os.getcwd()
 
     control_file = line.rstrip()
     results_file = control_file.replace('.control.txt', '.results.txt')
     log_file = control_file.replace('.control.txt', '.log.txt')
 
-    q_sub(['~/anavar1.2_23_05_17 {} {} {}'.format(control_file, results_file, log_file)],
-          out=control_file.replace('.control.txt', ''), t=2)
+    q_sub(['cd {}'.format(pwd),
+           '~/anavar1.2_23_05_17 {} {} {}'.format(control_file, results_file, log_file)],
+          out=pwd + '/' + control_file.replace('.control.txt', ''), t=2)
