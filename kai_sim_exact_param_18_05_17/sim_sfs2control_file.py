0403d785ea3e4fb15fc70d899c9035b277c9c45e
henryjuho <juhob@hotmail.co.uk>
2017-05-24 11:28:27 +0100

    added model to control name

--- kai_sim_exact_param_18_05_17/sim_sfs2control_file.py
+++ kai_sim_exact_param_18_05_17/sim_sfs2control_file.py
@@ -6,15 +6,20 @@ import sys
 # arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-control_file', help='control file to use as template', required=True)
+parser.add_argument('-model', choices=['full', 'reduced'], default='full')
+parser.add_argument('-out_dir', default='./')
 args = parser.parse_args()
 
 # variables
 c_file = args.control_file
+out_dir = args.out_dir
+model = args.model
 
 counter = 0
 for sfs_line in sys.stdin:
     counter += 1
-    with open(c_file[:c_file.rfind('.')] + '.rep' + str(counter) + '.control.txt', 'w') as out_control:
+    with open(out_dir + c_file[:c_file.rfind('.')] + '.rep' + str(counter) + '.' + model + '.control.txt', 'w') \
+            as out_control:
         for line in open(c_file):
             if line.startswith('sfs'):
                 out_control.write('sfs: ' + sfs_line)
