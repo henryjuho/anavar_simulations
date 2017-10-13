34f9f57dee48d9e669a9cf257a9457c2955f208c
henryjuho <juhob@hotmail.co.uk>
2017-10-13 16:26:13 +0100

    handles missing files due gsl errors

--- scripts/extract_best_result_3class.py
+++ scripts/extract_best_result_3class.py
@@ -3,6 +3,18 @@
 import argparse
 import os
 
+
+def both_files_pass(file1, file2):
+
+    for x in [file1, file2]:
+        length = len(open(x).readlines())
+
+        if length < 10:
+            return False
+
+    return True
+
+
 # arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-r_dir', help='Results file from anavar', required=True, action='append')
@@ -26,35 +38,44 @@ columns = ['run', 'imp', 'exit_code',
            'theta_3', 'gamma_3', 'e_3', 'lnL']
 
 # process files
-for x in r_files:
-    counter += 1
-    results_file = x[0]
-    rep = results_file.split('.')[-4]
-    model = results_file.split('.')[-3]
+for i in range(0, len(r_files), 2):
 
-    # extract and print best result
-    results = open(results_file).readlines()[4:6]
+    # check contents
+    paired_files = r_files[i: i+2]
 
-    header_line = results[0].rstrip('\n')
-    header = header_line.split('\t')
-    header_line += ('\tsim_theta_1\tsim_gamma_1\tsim_e_1\t'
-                    'sim_theta_2\tsim_gamma_2\tsim_e_2\t'
-                    'sim_theta_3\tsim_gamma_3\tsim_e_3\t'
-                    'model\trep')
-    hpos = {header[x]: x for x in range(0, len(header))}
+    if not both_files_pass(paired_files[0][0], paired_files[1][0]):
+        continue
 
-    if counter == 1:
-        print header_line
-    best_result = results[1].rstrip('\n').split('\t')
-    result_line = ''
-    for col in columns:
-        try:
-            result_line += best_result[hpos[col]] + '\t'
-        except KeyError:
-            result_line += 'NA\t'
+    for x in paired_files:
 
-    sim_output_info = '\t'.join([theta[0], gamma[0], e[0],
-                                 theta[1], gamma[1], e[1],
-                                 theta[2], gamma[2], e[2],
-                                 model, rep])
-    print(result_line + sim_output_info)
+        counter += 1
+        results_file = x[0]
+        rep = results_file.split('.')[-4]
+        model = results_file.split('.')[-3]
+
+        # extract and print best result
+        results = open(results_file).readlines()[4:6]
+
+        header_line = results[0].rstrip('\n')
+        header = header_line.split('\t')
+        header_line += ('\tsim_theta_1\tsim_gamma_1\tsim_e_1\t'
+                        'sim_theta_2\tsim_gamma_2\tsim_e_2\t'
+                        'sim_theta_3\tsim_gamma_3\tsim_e_3\t'
+                        'model\trep')
+        hpos = {header[x]: x for x in range(0, len(header))}
+
+        if counter == 1:
+            print header_line
+        best_result = results[1].rstrip('\n').split('\t')
+        result_line = ''
+        for col in columns:
+            try:
+                result_line += best_result[hpos[col]] + '\t'
+            except KeyError:
+                result_line += 'NA\t'
+
+        sim_output_info = '\t'.join([theta[0], gamma[0], e[0],
+                                     theta[1], gamma[1], e[1],
+                                     theta[2], gamma[2], e[2],
+                                     model, rep])
+        print(result_line + sim_output_info)
